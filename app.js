a55d082c9fa37f4dfbab140b9d94dece48f2fa9b
Paul Costa <paulcosta.student@careerdevs.com>
2018-12-18 00:54:48 +0000

    Get Elements By Class or Tag

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const titles = document.getElementsByClassName('title');
+var titles = document.getElementsByClassName('title');
 
-console.log(Array.isArray(titles));
+console.log(Array.isArray(titles));   ///false
+
 console.log(Array.isArray(Array.from(titles)));
+// turns html collection into array using .from
 
-Array.from(titles).forEach(function(title){
-  console.log(title);
+Array.from(titles).forEach(function(item){
+    console.log(item);
 });
+
+// const titles = document.getElementsByClassName('title');
+
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)));
+
+// Array.from(titles).forEach(function(title){
+//   console.log(title);
+// });
